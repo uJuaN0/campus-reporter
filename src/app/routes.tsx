3a310e8b49f76e5fa3aa874e46325f5d4388d413
Juan <nauj686@gmail.com>
2026-03-08 13:34:44 +0000

    fix github pages router basename

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -6,16 +6,21 @@ import { Eventos } from "./components/Eventos";
 import { Reportacoes } from "./components/Reportacoes";
 import { Reportar } from "./components/Reportar";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        { index: true, element: <Dashboard /> },
+        { path: "reportacoes", element: <Reportacoes /> },
+        { path: "achados-perdidos", element: <AchadosPerdidos /> },
+        { path: "eventos", element: <Eventos /> },
+        { path: "reportar", element: <Reportar /> }
+      ]
+    }
+  ],
   {
-    path: "/",
-    element: <Root />,
-    children: [
-      { index: true, element: <Dashboard /> },
-      { path: "reportacoes", element: <Reportacoes /> },
-      { path: "achados-perdidos", element: <AchadosPerdidos /> },
-      { path: "eventos", element: <Eventos /> },
-      { path: "reportar", element: <Reportar /> },
-    ],
-  },
-]);
+    basename: "/campus-reporter/"
+  }
+);
