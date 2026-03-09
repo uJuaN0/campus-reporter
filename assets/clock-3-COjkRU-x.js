0a1d08b5100505d27fd5cd13e0797aecf2f3bc71
Juan <nauj686@gmail.com>
2026-03-09 04:23:44 +0000

    Updates

--- assets/clock-3-COjkRU-x.js
+++
@@ -1,6 +0,0 @@
-import{c}from"./index-XKtU_fhk.js";/**
- * @license lucide-react v0.487.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polyline",{points:"12 6 12 12 16.5 12",key:"1aq6pp"}]],i=c("clock-3",o);export{i as C};
--- assets/textarea-5OkM-lVv.js
+++
@@ -1,6 +0,0 @@
-import{c as r,j as t,q as a}from"./index-XKtU_fhk.js";/**
- * @license lucide-react v0.487.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const n=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],s=r("plus",n);function d({className:e,...i}){return t.jsx("textarea",{"data-slot":"textarea",className:a("resize-none border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-input-background px-3 py-2 text-base transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",e),...i})}export{s as P,d as T};
